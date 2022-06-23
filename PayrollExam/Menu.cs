adedcbea2febd247f082366fbca64a4db8a5bd1f
vgiraldo176@misena.edu.co <Sena@DESKTOP-BD5HSN7>
2022-06-23 17:39:25 -0500

    first commit

---
+++ PayrollExam/Menu.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PayrollExam
+{
+    internal class Menu
+    {
+        public int NumberEmployee = 1;
+
+        public static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            Console.WriteLine("Welcome to the Employee Payroll");
+            Program program = new Program();
+            Data data = new Data();
+
+            int Option = 1;
+            try
+            {
+                do
+            {
+                Console.WriteLine("1) To add a employee");
+                Console.WriteLine("2) Exit the program");
+                Option = int.Parse(Console.ReadLine());
+                
+
+                    switch (Option)
+                    {
+
+
+
+                        case 1:
+                            {
+                                Console.WriteLine("Employee:" + menu.NumberEmployee);
+                                try
+                                {
+                                    program.DataEmploye();
+                                    if (data.Salary1 <= 0)
+                                    {
+                                        Console.WriteLine("Error, el salario no puede ser menor a 0");
+
+                                    }
+                                    else
+                                    {
+                                        program.ConsoleData();
+                                        program.FileData();
+                                    }
+                                }
+                               
+                                catch (FormatException e)
+                                {
+                                    Console.WriteLine("Invalid data.\nEnter the correct data", e);
+                                    Console.ReadKey();
+                                }
+                            
+                                break;
+
+                            }
+                        case 2:
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Completed program");
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Insert a correct number");
+                                break;
+                            }
+
+                            Console.WriteLine("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n");
+                            Console.Clear();
+                    }
+                    menu.NumberEmployee++;
+              
+            } while (Option != 2) ;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid data.\nEnter the correct data", e);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ PayrollExam/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PayrollExam
+{ 
+    internal class Program
+    {
+
+        Data data = new Data();
+        public void DataEmploye()
+
+        {
+           
+            try
+            {
+                    double SubTransport1 = 117.172;
+
+                Console.WriteLine("Dates Emplooyed");
+                Console.WriteLine("Insert Document: ");
+                data.Document1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Insert First Name: ");
+                data.FirstName1 = Console.ReadLine();
+                Console.WriteLine("Insert Last Name: ");
+                data.LastName1 = Console.ReadLine();
+                Console.WriteLine("Insert Salary: ");
+                data.Salary1 = double.Parse(Console.ReadLine());
+                
+              
+                    Console.WriteLine("Insert Worked Days");
+                    data.WorkedDays1 = double.Parse(Console.ReadLine());
+               
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid data.\nEnter the correct data", e);
+                Console.ReadKey();
+            }
+
+            if (data.WorkedDays1 <= 2000000)
+            { data.ResultTotal1 = data.Salary1 + data.SubTransport1; }
+            else
+            { data.ResultTotal1 = data.Salary1; }
+
+            data.Accrued1 = data.ResultTotal1 * data.WorkedDays1 / 30;
+
+            data.Heald1 = data.Accrued1 - 0.04;
+            data.Pension1 = data.Accrued1 - 0.04;
+
+        }
+
+        public void ConsoleData()
+        {
+            try
+            {
+                Console.WriteLine("Document: " + data.Document1);
+                Console.WriteLine("First Name: " + data.FirstName1);
+                Console.WriteLine("Last Name: " + data.LastName1);
+                Console.WriteLine("Salary: " + data.Salary1);
+                Console.WriteLine("Worked Days: " + data.WorkedDays1);
+                Console.WriteLine("Accrued: " + data.Accrued1);
+                Console.WriteLine("Health: " + data.Heald1);
+                Console.WriteLine("Pension: " + data.Pension1);
+            }catch (FormatException e)
+            
+                {
+                    Console.WriteLine("Invalid data.\nEnter the correct data", e);
+                    Console.ReadKey();
+            }
+
+        }
+
+        public void FileData()
+        {
+            TextWriter File = new StreamWriter("Payroll.txt", true);
+
+            File.WriteLine($"\n----------New Employee----------");
+            File.WriteLine("Document: " + data.Document1);
+            File.WriteLine("First Name: " + data.FirstName1);
+            File.WriteLine("Last Name: " + data.LastName1);
+            File.WriteLine("Salary: " + data.Salary1);
+            File.WriteLine("Worked Days: " + data.WorkedDays1);
+            File.WriteLine("Accrued: " + data.Accrued1);
+            File.WriteLine("Health: " + data.Heald1);
+            File.WriteLine("Pension: " + data.Pension1);
+
+            File.Close();
+        }
+
+    }
+}
